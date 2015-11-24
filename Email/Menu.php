d17a3bb2a6d4f51849392cb0f15f3c9e77aed1c9
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-11-24 12:47:28 -0500

    Add Menu entries separators

--- Email/Menu.php
+++ Email/Menu.php
@@ -11,18 +11,27 @@
 
 //add a Menu entry to the Grades module
 if ( $RosarioModules['Grades'] ) //verify Grades module is activated
+{
 	$menu['Grades']['admin'] += array(
+		5 => dgettext( 'Email', 'Email' ),
 		'Email/ReportCardsEmailParents.php' => dgettext( 'Email', 'Email Report Cards to Parents' ), 
 	);
+}
 
 //add a Menu entry to the Discipline module
 if ( $RosarioModules['Discipline'] ) //verify Discipline module is activated
+{
 	$menu['Discipline']['admin'] += array(
+		3 => dgettext( 'Email', 'Email' ),
 		'Email/DisciplineLogEmailParents.php' => dgettext( 'Email', 'Email Discipline Log to Parents' ), 
 	);
+}
 
 //add a Menu entry to the Student_Billing module
 if ( $RosarioModules['Student_Billing'] ) //verify Student_Billing module is activated
+{
 	$menu['Student_Billing']['admin'] += array(
+		2 => dgettext( 'Email', 'Email' ),
 		'Email/StudentBalancesEmailParents.php' => dgettext( 'Email', 'Email Student Balances to Parents' ), 
 	);
+}
